2db110fdf77a0e883c3134e78af656506a9773b9
Israel Sánchez <israelclarinete@gmail.com>
2024-05-23 18:32:37 +0200

    Contador hecho

---
+++ src/js/component/Counter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { CiClock2 } from "react-icons/ci";
+const Counter = (props) => {
+  return (
+    <div className="container-fluid col-8 d-flex justify-content-center bg-primary py-4 text-white rounded-3">
+      <CiClock2 className="display-1 text-center" />
+
+      <h1 className="m-3">{props.hoursV} h </h1>
+      <h1 className="m-3"> {props.minutesV} m</h1>
+      <h1 className="m-3"> {props.secondsV} s</h1>
+    </div>
+  );
+};
+
+Counter.propTypes = {
+  hoursV: PropTypes.number,
+  minutesV: PropTypes.number,
+  secondsV: PropTypes.number,
+};
+export default Counter;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,33 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-// include your styles into the webpack bundle
 import "../styles/index.css";
-
-//import your own components
 import Home from "./component/home.jsx";
 
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let counter = setInterval(startCount, 1000);
+
+function startCount() {
+  seconds++;
+  if (seconds == 60) {
+    seconds = 0;
+    minutes++;
+  }
+  if (minutes == 60) {
+    minutes = 0;
+    hours++;
+  }
+  displayTime(counter);
+}
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+function displayTime() {
+  ReactDOM.render(
+    <Home hoursV={hours} minutesV={minutes} secondsV={seconds} />,
+    document.querySelector("#app")
+  );
+}
+
+displayTime();
